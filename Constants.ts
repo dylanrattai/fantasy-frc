eb105cfdcb8ada30e6369e2fa22fa035551d2ccc
dylanrattai <d_rattai@yahoo.com>
2024-03-16 20:50:19 -0500

    setup vite project

---
+++ Constants.ts
@@ -0,0 +1,24 @@
+export const POINT_VALUES = Object.freeze({
+    EVENT_WINNER: 30,
+    EVENT_FINALIST: 20,
+    QUALS_UNDEFEATED: 25,
+    QUALS_WIN_LOSS_85_99: 15, // 85-99% Win loss ratio
+    QUALS_WIN_LOSS_70_84: 10, // 70-84% Win loss ratio
+    RP_AVG_35_40: 18, // 3.5-4 RP average
+    RP_AVG_30_34: 12, // 3-3.4 RP average
+    RP_AVG_20_29: 6, // 2-2.9 RP average
+    IN_EINSTEIN: 35,
+    EINSTEIN_FINALIST: 50,
+    WORLD_CHAMPS: 60,
+    WORLD_PERCENTILE_95TH: 20,
+    WORLD_PERCENTILE_90TH: 16,
+    WORLD_PERCENTILE_80TH: 12,
+    WORLD_PERCENTILE_70TH: 8,
+    MULTIPLIER_RP_1: 0.5, // Multiply the total RPs scored with this RP
+    MULTIPLIER_RP_2: 0.5, // Multiply the total RPs scored with this RP
+    MULTIPLIER_QUAL_TIES: 0.25,
+    MULTIPLIER_QUAL_WINS: 0.75,
+    MULTIPLIER_QUAL_LOSSES: 0.25,
+    MULTIPLIER_ELIM_WINS: 1,
+    MULTIPLIER_ELIM_LOSSES: 0.5
+})
